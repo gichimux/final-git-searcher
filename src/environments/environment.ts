bb3daedd49c62debec90a3cf30755f343cf15de8
gichimux <gichimumwai1@gmail.com>
2019-04-03 11:22:06 +0300

    added api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.github.com/users',
+/*224a278504de0617278fe7c7899223383ff77328 */
 };
 
 /*
