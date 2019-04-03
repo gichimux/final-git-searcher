e8753b0225750489a4f66a799c4e319c7a12787d
gichimux <gichimumwai1@gmail.com>
2019-04-03 11:14:59 +0300

    added services to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+
+import {RouterModule, Routes} from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { SearchService } from './search.service';
+import { NavComponent } from './nav/nav.component';
+
+
+const routes:Routes =[
+  {path:'Home', component:SearchComponent},
+  {path: "Search", component:NavComponent}
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule
+    
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
