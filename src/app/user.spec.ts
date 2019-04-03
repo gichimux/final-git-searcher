f84e9d3ad01220b932f6bd4ff409fce25dd65116
gichimux <gichimumwai1@gmail.com>
2019-04-03 11:09:53 +0300

    added angular generated module file

---
+++ src/app/user.spec.ts
@@ -0,0 +1,4 @@
+export class User{
+    
+  constructor(public login:string,public avatar_url:string,public public_repos:string,public followers:number,public following:number,public created_at:string){}
+  } 
